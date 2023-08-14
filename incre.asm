648c3b63377f60cdecef8c4b8f8cafafcff9759f
sigpher <425223341@qq.com>
2023-08-14 19:17:49 +0800

    incre

--- incre.asm
+++ incre.asm
@@ -1,16 +1,21 @@
 assume cs:code
 
 code segment
-         mov ax,0ffffh
-         mov ds,ax
-         mov cx,000bh
-         mov dx,0
-         mov bx,0
-    s:   mov dl,[bl]
-         mov dh,0
+         mov  ax,0ffffh
+         mov  ds,ax
+         mov  cx,000bh
+         mov  dx,0
+         mov  bx,0
+    s:   mov  al,[bx]
+         mov  ah,0
+         add  dx,ax
+         INC  bx
+            
+         LOOP s
+         
         
 
-         mov ax,4c00h
-         int 21h
+         mov  ax,4c00h
+         int  21h
 code ends
 end
